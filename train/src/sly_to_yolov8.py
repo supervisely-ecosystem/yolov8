d31e2dae25d5ddf7a62d2975f32e32de00e083fa
MaxTeselkin <tesmaxxxx@gmail.com>
2023-06-21 03:20:12 +0400

    instance segmentation convertation format bug fix

--- train/src/sly_to_yolov8.py
+++ train/src/sly_to_yolov8.py
@@ -31,31 +31,23 @@ def _transform_label(class_names, img_size, label: sly.Label, task_type):
         result = f"{class_number} {x_center} {y_center} {width} {height} {keypoints_str}"
     elif task_type == "instance segmentation":
         class_number = class_names.index(label.obj_class.name)
-        rect_geometry = label.geometry.to_bbox()
-        center = rect_geometry.center
-        x_center = round(center.col / img_size[1], 6)
-        y_center = round(center.row / img_size[0], 6)
-        width = round(rect_geometry.width / img_size[1], 6)
-        height = round(rect_geometry.height / img_size[0], 6)
-        if label.geometry.geometry_name() == "bitmap":
-            contours = label.geometry.to_contours()
-            points = [contour.exterior_np for contour in contours]
-            points[0] = np.flip(points[0], axis=1)
-            scaled_points = []
-            for point in points[0]:
-                scaled_points.append(round(point[0] / img_size[1], 6))
-                scaled_points.append(round(point[1] / img_size[0], 6))
-            scaled_points_str = " ".join(str(point) for point in scaled_points)
-            result = f"{class_number} {x_center} {y_center} {width} {height} {scaled_points_str}"
-        elif label.geometry.geometry_name() == "polygon":
+        if type(label.geometry) is sly.Bitmap:
+            new_obj_class = sly.ObjClass(label.obj_class.name, sly.Polygon)
+            labels = label.convert(new_obj_class)
+            for i, label in enumerate(labels):
+                if i == 0:
+                    points = label.geometry.exterior_np
+                else:
+                    points = np.concatenate((points, label.geometry.exterior_np), axis=0)
+        else:
             points = label.geometry.exterior_np
-            points = np.flip(points, axis=1)
-            scaled_points = []
-            for point in points:
-                scaled_points.append(round(point[0] / img_size[1], 6))
-                scaled_points.append(round(point[1] / img_size[0], 6))
-            scaled_points_str = " ".join(str(point) for point in scaled_points)
-            result = f"{class_number} {x_center} {y_center} {width} {height} {scaled_points_str}"
+        points = np.flip(points, axis=1)
+        scaled_points = []
+        for point in points:
+            scaled_points.append(round(point[0] / img_size[1], 6))
+            scaled_points.append(round(point[1] / img_size[0], 6))
+        scaled_points_str = " ".join([str(point) for point in scaled_points])
+        result = f"{class_number} {scaled_points_str}"
     return result
 
 
@@ -123,15 +115,11 @@ def _transform_set(set_name, data_yaml, project_meta, items, progress_cb, task_t
     classes_names = data_yaml["names"]
 
     used_names = set()
-    with progress_cb(
-        message=f"Converting {set_name} set to YOLOv8 format...", total=len(items)
-    ) as pbar:
+    with progress_cb(message=f"Converting {set_name} set to YOLOv8 format...", total=len(items)) as pbar:
         for batch in sly.batched(items, batch_size=max(int(len(items) / 50), 10)):
             for item in batch:
                 ann = sly.Annotation.load_json_file(item.ann_path, project_meta)
-                _item_name = sly._utils.generate_free_name(
-                    used_names, sly.fs.get_file_name(item.name)
-                )
+                _item_name = sly._utils.generate_free_name(used_names, sly.fs.get_file_name(item.name))
                 used_names.add(_item_name)
 
                 _ann_name = f"{_item_name}.txt"
