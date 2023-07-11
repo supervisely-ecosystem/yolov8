a144b898625408e0e7203e0a01da0af1d569e02f
MaxTeselkin <tesmaxxxx@gmail.com>
2023-07-11 20:08:20 +0400

    experiment

--- train/src/main.py
+++ train/src/main.py
@@ -1,4 +1,5 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from pathlib import Path
 import numpy as np
 import yaml
@@ -75,7 +76,6 @@ load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 api = sly.Api()
 team_id = sly.env.team_id()
-os.environ["CUDA_VISIBLE_DEVICES"] = 0
 
 # if app had started from context menu, one of this has to be set:
 project_id = sly.env.project_id(raise_not_found=False)
