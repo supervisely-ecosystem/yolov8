e487bc32903a7f8fdca9d71bd6f9576dc61311b4
almaz <almazgimaev@gmail.com>
2024-08-22 15:00:58 +0200

    update result path

--- train/src/utils.py
+++ train/src/utils.py
@@ -81,7 +81,7 @@ def custom_plot(self, normalize=True, save_dir="", names=(), on_plot=None):
 
 def get_eval_results_dir_name(api, task_id, project_info):
     task_info = api.task.get_info_by_id(task_id)
-    task_dir = f"{task_id}_task_{task_info['meta']['app']['name']}"
+    task_dir = f"{task_id}_{task_info['meta']['app']['name']}"
     eval_res_dir = f"/model-benchmark/evaluation/{project_info.id}_{project_info.name}/{task_dir}/"
     eval_res_dir = api.storage.get_free_dir_name(sly.env.team_id(), eval_res_dir)
     return eval_res_dir
