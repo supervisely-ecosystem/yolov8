b553b304a0faf48ee946708feec658278f27e30e
almaz <almazgimaev@gmail.com>
2024-09-20 19:33:40 +0200

    reload split widget

--- train/src/main.py
+++ train/src/main.py
@@ -1401,6 +1401,7 @@ def start_training():
     try:
         train_val_split._project_fs = sly.Project(g.project_dir, sly.OpenMode.READ)
         train_val_split._project_id = None
+        train_val_split_area.reload()
         train_set, val_set = train_val_split.get_splits()
         train_val_split._project_id = project_id
     except Exception:
@@ -1419,6 +1420,7 @@ def start_training():
         )
         train_val_split._project_fs = sly.Project(g.project_dir, sly.OpenMode.READ)
         train_val_split._project_id = None
+        train_val_split_area.reload()
         train_set, val_set = train_val_split.get_splits()
         train_val_split._project_id = project_id
     verify_train_val_sets(train_set, val_set)
