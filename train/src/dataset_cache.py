afda8f57d72da3b73a9317f888b052416b76bf7e
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-11-15 00:06:41 -0300

    bugfixes

--- train/src/dataset_cache.py
+++ train/src/dataset_cache.py
@@ -7,7 +7,6 @@ import src.globals as g
 from supervisely.project.download import (
     download_to_cache,
     copy_from_cache,
-    is_cached,
     get_cache_size,
 )
 from supervisely.project.project import Dataset
@@ -48,6 +47,7 @@ def _no_cache_download(
                 progress_cb=pbar.update,
                 save_images=True,
                 save_image_info=True,
+                resume_download=True,
             )
 
 
@@ -79,36 +79,11 @@ def download_project(
     use_cache: bool,
     progress: Progress,
 ):  
-    if api.server_address.startswith("https://"):
-        semaphore = asyncio.Semaphore(100)
-    else:
-        semaphore = None
     if os.path.exists(g.project_dir):
         sly.fs.clean_dir(g.project_dir)
     if not use_cache:
-        return _no_cache_download(api, project_info, dataset_infos, progress, semaphore=semaphore)
+        return _no_cache_download(api, project_info, dataset_infos, progress)
     try:
-        # get datasets to download and cached
-        to_download = [
-            info for info in dataset_infos if not is_cached(project_info.id, info.name)
-        ]
-        cached = [
-            info for info in dataset_infos if is_cached(project_info.id, info.name)
-        ]
-        if len(cached) == 0:
-            log_msg = "No cached datasets found"
-        else:
-            log_msg = "Using cached datasets: " + ", ".join(
-                f"{ds_info.name} ({ds_info.id})" for ds_info in cached
-            )
-        sly.logger.info(log_msg)
-        if len(to_download) == 0:
-            log_msg = "All datasets are cached. No datasets to download"
-        else:
-            log_msg = "Downloading datasets: " + ", ".join(
-                f"{ds_info.name} ({ds_info.id})" for ds_info in to_download
-            )
-        sly.logger.info(log_msg)
         # get images count
         total = sum([ds_info.images_count for ds_info in dataset_infos])
         # download
@@ -119,7 +94,6 @@ def download_project(
                 dataset_infos=dataset_infos,
                 log_progress=True,
                 progress_cb=pbar.update,
-                semaphore=semaphore,
             )
         # copy datasets from cache
         total = sum([get_cache_size(project_info.id, _get_dataset_path(api, dataset_infos, ds.id)) for ds in dataset_infos])
@@ -143,4 +117,4 @@ def download_project(
         )
         if os.path.exists(g.project_dir):
             sly.fs.clean_dir(g.project_dir)
-        _no_cache_download(api, project_info, dataset_infos, progress, semaphore=semaphore)
+        _no_cache_download(api, project_info, dataset_infos, progress)
