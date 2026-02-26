6f30ebea8405e216b4c2da4cb494aad9878c4d51
vorozhkog <vorozhkog@icloud.com>
2026-02-26 22:46:00 +0000

    Update image tag to hardened-dev02 in publish script

--- docker/publish.sh
+++ docker/publish.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 IMAGE_NAME=supervisely/yolov8
-IMAGE_TAG=hardened-dev01
+IMAGE_TAG=hardened-dev02
 IMAGE_REF="${IMAGE_NAME}:${IMAGE_TAG}"
 
 docker build -t "${IMAGE_REF}" . && \
