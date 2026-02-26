80dc3b73240a1a365fcb8c1736617622c5239a14
vorozhkog <vorozhkog@icloud.com>
2026-02-26 23:58:28 +0000

    upd to dev04

--- docker/publish.sh
+++ docker/publish.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 IMAGE_NAME=supervisely/yolov8
-IMAGE_TAG=hardened-dev03
+IMAGE_TAG=hardened-dev04
 IMAGE_REF="${IMAGE_NAME}:${IMAGE_TAG}"
 
 docker build -t "${IMAGE_REF}" . && \
