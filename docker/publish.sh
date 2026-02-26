a789eed43bd67be165a11efff1f2118ab3b3b11b
vorozhkog <vorozhkog@icloud.com>
2026-02-26 21:25:34 +0000

    Update image tag in publish script to include version suffix

--- docker/publish.sh
+++ docker/publish.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 IMAGE_NAME=supervisely/yolov8
-IMAGE_TAG=hardened-dev
+IMAGE_TAG=hardened-dev-01
 IMAGE_REF="${IMAGE_NAME}:${IMAGE_TAG}"
 
 docker build -t "${IMAGE_REF}" . && \
